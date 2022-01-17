810cbd3ffa1d24a28be8aac9ad5d0e0c43986314
shaktidewanggaa <90263142+shaktidewanggaa@users.noreply.github.com>
2022-01-17 13:51:39 +0700

    Create family.java

---
+++ family.java
@@ -0,0 +1,14 @@
+public class Family {
+    
+    protected String fathersName;
+    protected String mothersName;
+
+    public void setfathersName(String fathersName) {
+        this.fathersName = fathersName;
+    }
+
+    public String getfathersName() {
+        return this.fathersName;
+    }
+   
+}
